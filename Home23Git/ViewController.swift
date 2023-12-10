094431ac940ae04ce3a77334e2b13f8601b25e67
Aleksandr <a.folyak2000@gmail.com>
2023-12-10 19:33:40 +0600

    Initial Commit

---
+++ Home23Git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home23Git
+//
+//  Created by Александр Фоляк on 10.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

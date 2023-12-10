fb1d4163d9059ec7d2dec094caf5c2eedfcfd14b
Aleksandr <a.folyak2000@gmail.com>
2023-12-10 20:08:45 +0600

    added button and its settings for alert

--- Home23Git/ViewController.swift
+++ Home23Git/ViewController.swift
@@ -1,19 +1,44 @@
-//
-//  ViewController.swift
-//  Home23Git
-//
-//  Created by Александр Фоляк on 10.12.2023.
-//
-
+// MARK: - Фоляк Александр
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    // MARK: Private properties
+    private let showAlertWithButton = UIButton()
+    
+    // MARK: - Lyfe cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureButton()
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        styleButton()
+    }
+    
+    // MARK: - API
+    private func configureButton() {
+        view.addSubview(showAlertWithButton)
+        
+        showAlertWithButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            showAlertWithButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            showAlertWithButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            showAlertWithButton.widthAnchor.constraint(equalToConstant: 250)
+        ])
+        showAlertWithButton.addTarget(self, action: #selector(clickToButton), for: .touchUpInside)
+    }
+    @objc private func clickToButton() {
+        let alertController = UIAlertController(title: "Приветствую!", message: "Вам необходимо заполнить данные ниже 👇", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alertController, animated: true)
+    }
+    private func styleButton() {
+        showAlertWithButton.setTitle("Что здесь происходит?", for: .normal)
+        showAlertWithButton.setTitleColor(.white, for: .normal)
+        showAlertWithButton.backgroundColor = .darkGray
+        showAlertWithButton.layer.cornerRadius = showAlertWithButton.frame.height / 2
+    }
 }
-
